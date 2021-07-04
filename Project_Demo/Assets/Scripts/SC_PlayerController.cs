4babbbe31e02705c447a869e11c92f3481c43fe3
Imendezl <imendezlora94@gmail.com>
2021-07-04 19:06:58 +0200

    Movement and Dash Player

--- Project_Demo/Assets/Scripts/SC_PlayerController.cs
+++ Project_Demo/Assets/Scripts/SC_PlayerController.cs
@@ -4,15 +4,93 @@ using UnityEngine;
 
 public class SC_PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float speed, multDashSpeed, startDashTime, betweenDashTime;
+
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity, moveInput;
+    private float dashTime, dashCountdown;
+    private bool dashing, startDashCountdown;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        dashing = false;
+        dashCountdown = betweenDashTime;
+
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        PlayerMove();
+
+        if (Input.GetButton("Jump") && dashCountdown >= betweenDashTime)
+        {
+            if (!dashing)
+            {
+                DashMove();
+                startDashCountdown = true;
+            }
+
+        }
+        else
+        {
+            //Dash time
+            if (dashTime <= 0)
+            {
+                dashing = false;
+                dashTime = startDashTime;
+
+            }
+            else
+            {
+                dashTime -= Time.deltaTime;
+
+            }
+        }
+
     }
+
+    void FixedUpdate()
+    {
+        
+        //Dash and Movement 
+        if (dashing)
+        {
+            rb.MovePosition(rb.position + (moveVelocity * multDashSpeed) * Time.fixedDeltaTime);
+        }
+        else
+        {
+            rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        }
+
+        //Countdown to do dash again
+        if (startDashCountdown)
+        {
+            dashCountdown -= Time.fixedDeltaTime;
+            if (dashCountdown <= 0)
+            {
+                dashCountdown = betweenDashTime;
+                startDashCountdown = false;
+            }
+        }
+
+    }
+
+//Preparing Dash
+    void DashMove()
+    {
+        dashing = true;
+        dashTime = startDashTime;
+    }
+
+//Movement direction
+    void PlayerMove()
+    {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput.normalized * speed;
+    }
+
 }
